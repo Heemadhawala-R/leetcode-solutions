29ea2ce8a80be9bebeee7a787b87ac9d958266af
HEEMADHAWALA R <186943648+Heemadhawala-R@users.noreply.github.com>
2026-03-14 11:36:35 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.89%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.c
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.c
@@ -5,14 +5,14 @@ int compare(const void * a , const void *b){
 int largestInteger(int num) {
     int digits[10],even[10], odd[10];
     int digi_count=0,even_count=0,odd_count=0;
-    int temp=num;
-    while(temp>0){
-        digits[digi_count++] = temp % 10;
+    int copy=num;
+    while(copy>0){
+        digits[digi_count++] = copy % 10;
             if(digits[digi_count-1] % 2 == 0)
             even[even_count++] = digits[digi_count-1];
         else
             odd[odd_count++] = digits[digi_count-1];
-        temp /= 10;
+        copy /= 10;
     }
     qsort(even, even_count, sizeof(int), compare);
     qsort(odd, odd_count, sizeof(int), compare);
